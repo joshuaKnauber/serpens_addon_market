e112f4f5629df0efc593f72e79d046fcd468f58c
BuildTools <finn.knauber@gmail.com>
2020-10-17 22:39:43 +0200

    Serverlog

--- bot.py
+++ bot.py
@@ -101,22 +101,21 @@ async def on_message(message):
                     # if exists in addons.json
                     if addon_exists(user_id, find_open_entry(user_id)["name"]):
                         # delete old data
-                        if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), find_open_entry(user_id)["url"])):
-                            os.remove(os.path.dirname(os.path.realpath(__file__)) + find_open_entry(user_id)["url"])
+                        # if os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), find_open_entry(user_id)["url"])):
+                        #     os.remove(os.path.dirname(os.path.realpath(__file__)) + find_open_entry(user_id)["url"])
 
                         remove_addon(user_id, find_open_entry(user_id)["name"])
                         await message.channel.send("<@" + str(message.author.id) + "> Updated your old addon!")
                     else:
                         await message.channel.send("<@" + str(message.author.id) + "> Added your addon to marketplace!")
-                    
+
                     # save file
-                    await addon_file.save(addon_file.filename.split(".")[0] + str(user_id) + ".py")
+                    await addon_file.save("./addons/" + addon_file.filename.split(".")[0] + str(user_id) + ".py")
 
                     # put open entry and reference to file in addons.json
                     new_entry = find_open_entry(user_id)
                     new_entry["url"] = "/" + addon_file.filename.split(".")[0] + str(user_id) + ".py"
                     add_addon(new_entry)
-
                     
                     # remove open entry
                     remove_open_entry(user_id)
